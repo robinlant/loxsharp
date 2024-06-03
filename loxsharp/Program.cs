f2028da3f051d167560a762668abd088d33c5ce4
@robinlant <mtarasovets08@gmail.com>
2024-06-03 23:12:19 +0200

    implement simple console app

---
+++ loxsharp/Program.cs
@@ -0,0 +1,57 @@
+﻿using System.Text;
+using Environment = System.Environment;
+
+namespace loxsharp;
+
+public class Program
+{
+	public static void Main(string[] args)
+	{
+		Console.WriteLine();
+		switch (args.Length)
+		{
+			case > 1:
+				Console.WriteLine("Usage: loxsharp [script]");
+				Environment.Exit(64);
+				break;
+			case 1:
+				RunFile(args[0]);
+				break;
+			default:
+				RunPrompt();
+				break;
+		}
+	}
+
+	private static void RunFile(string file)
+	{
+		if (!File.Exists(file))
+		{
+			Console.WriteLine(
+				$"Cant open the file: {file}." +
+				$"\nFile doesnt exist.");
+			Environment.Exit(66);
+		}
+
+		var bytes = File.ReadAllBytes(file);
+
+		Run(Encoding.UTF8.GetString(bytes));
+	}
+
+	private static void RunPrompt()
+	{
+		for (;;)
+		{
+			Console.Write("> ");
+			var line = Console.ReadLine();
+			if (line is null) break;
+			Run(line);
+		}
+	}
+
+	private static void Run(string source)
+	{
+		// var scanner = new Scanner();
+		// var tokens = Scanner.scanTockens(source);
+	}
+}
