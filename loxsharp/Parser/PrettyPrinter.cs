1bcc98a39dbbf04d18ba85d27126e90e05e2f64b
@robinlant <mtarasovets08@gmail.com>
2024-06-08 22:55:47 +0200

    implement PrettyPrinter

---
+++ loxsharp/Parser/PrettyPrinter.cs
@@ -0,0 +1,47 @@
+using System.Text;
+using loxsharp.Parser.Productions;
+
+namespace loxsharp.Parser;
+
+public class AstPrinter : ISyntaxTreeVisitor<String>
+{
+	public string VisitBinary(Binary binary)
+	{
+		return Parenthesize(binary.Token.Lexeme, binary.Left, binary.Right);
+	}
+
+
+	public string VisitGrouping(Grouping grouping)
+	{
+		return Parenthesize("group", grouping.Expression);
+	}
+
+	public string VisitLiteral(Literal literal)
+	{
+		if (literal.Value is null) return "nil";
+
+		return literal.Value.ToString();
+
+	}
+
+	public string VisitUnary(Unary unary)
+	{
+		return Parenthesize(unary.Token.Lexeme, unary.Right);
+	}
+
+	private string Parenthesize(string name, params Expr[] exprs)
+	{
+		var strBuilder = new StringBuilder();
+
+		strBuilder.Append('(')
+			.Append(name);
+
+		foreach (var i in exprs)
+		{
+			strBuilder.Append(' ')
+				.Append(i.Accept(this));
+		}
+
+		return strBuilder.ToString();
+	}
+}
